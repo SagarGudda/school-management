19ef6169cb381afe8ba8d12aa372849efc0e6721
SagarGudda <sagargudda@gmail.com>
2023-12-13 23:18:30 +0530

    teacher service code changes to test and deploy to aws

--- teacher-service/src/main/java/com/school/management/teacher/TeacherserviceApplication.java
+++ teacher-service/src/main/java/com/school/management/teacher/TeacherserviceApplication.java
@@ -2,10 +2,9 @@ package com.school.management.teacher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class TeacherserviceApplication {
 
 	public static void main(String[] args) {
